14d12321864bdf16adbcbf7500622da7f186ffea
Greg Brilev <gbrilev@gmail.com>
2020-05-28 18:25:52 +0300

    completely rewritten Closes #1

--- gogogo.go
+++ gogogo.go
@@ -8,63 +8,77 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 func main() {
-	//
-	var unexpected = func(err error) {
-		if err != nil {
-			panic(err)
-		}
-	}
-	//
-	var investigateURL = func(url string, out chan<- int) {
-		resp, e := http.Get(url)
-		unexpected(e)
-		defer resp.Body.Close()
-		body, e := ioutil.ReadAll(resp.Body)
-		unexpected(e)
-		var c = strings.Count(string(body), "Go")
-		fmt.Printf("Count for %s: %d\n", url, c)
-		out <- c
-	}
-
-	const POOL_SIZE = 5
-	var (
-		// результат
-		total = 0
-		// живых горутин
-		thingsToDo = 0
-		// случился ли конец URLам
-		isEOF = false
-		//
-		ch = make(chan int)
-	)
-	stdin := bufio.NewReader(os.Stdin)
-
-mainCicle:
-	for {
-		if isEOF == false {
+	var producer = func(outch chan<- string) {
+		defer close(outch)
+		stdin := bufio.NewReader(os.Stdin)
+	readLoop:
+		for {
 			switch url, err := stdin.ReadString('\n'); err {
 			case nil:
-				thingsToDo += 1
-				go investigateURL(strings.ReplaceAll(url, "\n", ""), ch)
+				outch <- strings.ReplaceAll(url, "\n", "")
 			case (io.EOF):
-				isEOF = true
+				break readLoop
 			default:
 				panic(err)
 			}
 		}
-		if thingsToDo == POOL_SIZE || (isEOF && thingsToDo > 0) {
-			v := <-ch
-			total += v
-			thingsToDo -= 1
-		}
-		if isEOF && thingsToDo == 0 {
-			break mainCicle
+	}
+	var investigator = func(inch <-chan string, outch chan<- int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for url := range inch {
+			resp, err := http.Get(url)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			var c = strings.Count(string(body), "Go")
+			fmt.Printf("Count for %s: %d\n", url, c)
+			outch <- c
 		}
 	}
-
-	close(ch)
+	var dispatcher = func(inch <-chan string, outch chan<- int) {
+		defer close(outch)
+		const POOL_SIZE = 5
+		var (
+			spawned = 0
+			tasks   = make(chan string)
+			wg      = sync.WaitGroup{}
+		)
+		for url := range inch {
+			if spawned < POOL_SIZE {
+				spawned += 1
+				wg.Add(1)
+				go investigator(tasks, outch, &wg)
+			}
+			tasks <- url
+		}
+		close(tasks)
+		wg.Wait()
+	}
+	var consumer = func(inch <-chan int) int {
+		n := 0
+		for x := range inch {
+			n += x
+		}
+		return n
+	}
+	// каналы
+	var urls = make(chan string)
+	var counts = make(chan int)
+	// конвейер
+	// пушит урлы в urls
+	go producer(urls)
+	// слушает urls, создает горутины для обработки, отправляет ответы от них дальше в counts
+	go dispatcher(urls, counts)
+	// редьюсит ответы
+	total := consumer(counts)
 	fmt.Println("Total:", total)
 }
