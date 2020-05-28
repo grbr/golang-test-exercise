ee84ec19c1595a3b339423b68fd5cabca96ea53a
Greg Brilev <gbrilev@gmail.com>
2020-05-28 18:27:18 +0300

    some other cases

--- example.sh
+++ example.sh
@@ -2,3 +2,12 @@
 # 6x:
 echo -e 'https://golang.org\nhttps://golang.org\nhttps://golang.org\nhttps://golang.org\nhttps://golang.org\nhttps://golang.org' |\
 go run gogogo.go
+# 5x:
+echo -e 'https://golang.org\nhttps://golang.org\nhttps://golang.org\nhttps://golang.org\nhttps://golang.org' |\
+go run gogogo.go
+# 2x:
+echo -e 'https://golang.org\nhttps://golang.org' |\
+go run gogogo.go
+# 1x:
+echo -e 'https://golang.org' |\
+go run gogogo.go
